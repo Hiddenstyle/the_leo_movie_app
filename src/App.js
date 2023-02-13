cf67ba505dabbffe648d3c281c0483e66d576ee4
André Teilmann <andre@hiddenstyle.com>
2023-02-13 23:53:30 +0100

    Added loading routines.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import useFetch from './customHooks/useFetch';
 
 const App = () => {
+
+  const {loadedData} = useFetch("https://api.themoviedb.org/3/search/movie?api_key=6a7b1e38fa6f12bacae44de83e6d1908&query=goonies");
+
   return (
     <div className='appWrapper'>
       <Header />
-      <MovieList/>
+      {loadedData && <MovieList movieData={loadedData}/>}
     </div>
   )
 }
