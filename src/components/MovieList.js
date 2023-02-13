bc00fa72140f8350894b85a19990a620d5b11831
André Teilmann <andre@hiddenstyle.com>
2023-02-13 23:55:43 +0100

    Added basic diplay of info on loaded movies.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,19 @@
-const MovieList = () => {
-    return ( 
-        console.log("MovieList component")
-    );
+const MovieList = ({movieData}) => {
+    return(
+        <div className="movieList">
+            {
+                movieData.map(
+                    (movie) => (
+                        <div className="movieItem" key={movie.id}>
+                            <h1>{movie.title}</h1>
+                            <h4>{movie.release_date.substr(0,4)}</h4>
+                            <p>{movie.overview}</p>
+                        </div>
+                    )
+                )
+            }
+        </div>    
+    )
 }
- 
+
 export default MovieList
