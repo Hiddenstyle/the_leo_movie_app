ac2057d7f2b196ebf587c2d72f382874777849c5
André Teilmann <andre@hiddenstyle.com>
2023-02-14 01:51:04 +0100

    Added getData function to pass search term to useFetch.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import useFetch from './customHooks/useFetch';
 
 const App = () => {
 
-  const {loadedData} = useFetch("https://api.themoviedb.org/3/search/movie?api_key=6a7b1e38fa6f12bacae44de83e6d1908&query=goonies");
+  const [data, setData] = useState(null);
+  
+  const getData = (quearyData) => {
+    setData("https://api.themoviedb.org/3/search/movie?api_key=6a7b1e38fa6f12bacae44de83e6d1908&query=" + quearyData)
+  }
+
+  const {loadedData} = useFetch(data);
 
   return (
     <div className='appWrapper'>
-      <Header />
-      {loadedData && <MovieList movieData={loadedData}/>}
+      <Header getData={getData}/>
+     {loadedData && <MovieList movieData={loadedData}/>}
     </div>
   )
 }
