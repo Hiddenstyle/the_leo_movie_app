62e395e27b0d5c205593e4f4c5653ede488204ec
André Teilmann <andre@hiddenstyle.com>
2023-02-14 18:56:15 +0100

    Added basic favorite setup.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,6 +3,10 @@ const MovieList = ({movieData}) => {
 
     const POSTER_URL = "https://image.tmdb.org/t/p/w200";
 
+    const toggleFav = () => {
+        console.log("Click");
+    }
+
     return(
         <div className="movieList">
             {
@@ -11,6 +15,7 @@ const MovieList = ({movieData}) => {
                         <div className="movieItem" key={movie.id}>
                             <img className="poster" src={POSTER_URL + movie.poster_path} alt=""></img>
                             <h1 className="title">{movie.title}</h1>
+                            <div className="material-symbols-outlined" onClick={toggleFav}>favorite</div>
                         </div>
                     )
                 )
